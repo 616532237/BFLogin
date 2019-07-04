2b5cd49ea2648e958dd3a3d7c9e08a0d74bf6222
qifeibo <616532237@qq.com>
2019-07-04 14:35:56 +0800

    Initial Commit

---
+++ BFLogin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BFLogin
+//
+//  Created by 齐飞薄 on 2019/7/4.
+//  Copyright © 2019 BF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BFLogin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BFLogin
+//
+//  Created by 齐飞薄 on 2019/7/4.
+//  Copyright © 2019 BF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
